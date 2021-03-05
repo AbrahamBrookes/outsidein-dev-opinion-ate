35b31c7dc34030fb0dae9b16ce42cc59ea90645b
Abraham Brookes <abrahambrookes@gmail.com>
2021-03-06 09:04:36 +1000

    updated RestaurantsList to display data with unit tests

--- src/components/RestaurantsList.vue
+++ src/components/RestaurantsList.vue
@@ -1,12 +1,26 @@
 <template>
 	<div>
 		<h2>Restaurants list</h2>
+        <ul>
+            <li 
+                v-for="restaurant in restaurants"
+                :key="restaurant.id"
+                data-testid="restaurant"
+            >
+                {{ restaurant.name }}
+            </li>
+        </ul>
 	</div>
 </template>
 
 <script>
-import {mapActions} from 'vuex'
+import {mapActions, mapState} from 'vuex'
 export default {
+    computed: {
+        ...mapState({
+            restaurants: state => state.restaurants.records,
+        })
+    },
     methods: {
         ...mapActions({
             loadRestaurants: 'restaurants/load'
--- tests/unit/components/RestaurantsList.spec.js
+++ tests/unit/components/RestaurantsList.spec.js
@@ -4,25 +4,52 @@ import {mount, createLocalVue} from '@vue/test-utils'
 import RestaurantsList from '@/components/RestaurantsList'
 
 describe('RestaurantsList', () => {
+	// first set up the environment for our tests
 	const localVue = createLocalVue()
 	localVue.use(Vuex)
-
-	it('loads restaurants on mount', () => {
-		const restaurantsModule = {
-			namespaced: true,
-			actions: {
-				load: jest.fn().mockName('load'),
-			}
+	// a mock modue that will be used by the component we are testing
+	const restaurantsModule = {
+		namespaced: true,
+		state: {
+			records: [
+				{
+					id: 1,
+					name: 'Sushi Place',
+				},
+				{
+					id: 2,
+					name: 'Pizza Place',
+				},
+			]
+		},
+		actions: {
+			load: jest.fn().mockName('load'),
 		}
-
-		const store = new Vuex.Store({
-			modules: {
-				restaurants: restaurantsModule,
-			}
-		})
-
-		mount(RestaurantsList, {localVue, store})
-
+	}
+	// load up our test store with the mock module 
+	const store = new Vuex.Store({
+		modules: {
+			restaurants: restaurantsModule,
+		}
+	})
+	// mount the component we are testing to our local environment
+	const wrapper = mount(RestaurantsList, {localVue, store})
+	
+	// run some tests on it!
+	it('loads restaurants on mount', () => {	
 		expect(restaurantsModule.actions.load).toHaveBeenCalled()
+	})
+
+	it('displays a list of restaurants', () => {
+		const firstRestaurantName = wrapper
+			.findAll('[data-testid="restaurant"]')
+			.at(0)
+			.text()
+		expect(firstRestaurantName).toBe('Sushi Place')
+		const secondRestaurantName = wrapper
+			.findAll('[data-testid="restaurant"]')
+			.at(1)
+			.text()
+		expect(secondRestaurantName).toBe('Pizza Place')
 	})
 })
