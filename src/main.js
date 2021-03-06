fff50e8058225b079d7d2c8d933e8fe6eba044c5
Abraham Brookes <abrahambrookes@gmail.com>
2021-03-06 16:11:32 +1000

    added api client wrapper around axios

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
-// eslint-disable-next-line
-const heroku_api_key = '1xYU87pyIfSPT9t9Pwrlv2QooZowK3iQ'
-
 Vue.config.productionTip = false
 
 new Vue({
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// our cruft
+import api from '@/store/api'
+
 // modules
 import restaurants from './modules/restaurants'
 
@@ -11,6 +14,6 @@ export default new Vuex.Store({
 	mutations: {},
 	actions: {},
 	modules: {
-		restaurants: restaurants(),
+		restaurants: restaurants(api),
 	},
 })
