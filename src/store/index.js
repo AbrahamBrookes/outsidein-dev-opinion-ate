1d7117e84134429aacccdee9182b9e4b3e0c7169
Abraham Brookes <abrahambrookes@gmail.com>
2021-03-06 15:15:50 +1000

    added restaurants module and unit test

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// modules
+import restaurants from './modules/restaurants'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
 	state: {},
 	mutations: {},
 	actions: {},
-	modules: {},
+	modules: {
+		restaurants: restaurants(),
+	},
 })
