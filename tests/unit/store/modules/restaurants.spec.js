cd5691b38f821c006ac829495625a500c489e311
Abraham Brookes <abrahambrookes@gmail.com>
2021-03-07 21:05:49 +1000

    added unit test for restaurant module loading flag

--- tests/unit/store/modules/restaurants.spec.js
+++ tests/unit/store/modules/restaurants.spec.js
@@ -7,7 +7,24 @@ describe('restaurants', () => {
 	const localVue = createLocalVue()
 	localVue.use(Vuex)
 
-	describe('load action', () => {
+    describe('while loading', () => {
+        // create a store witha stubbed api
+        const api = {
+            // this promise doesn't resolve
+            loadRestaurants: () => new Promise(() => {}),
+        }
+        const store = new Vuex.Store({
+            modules: {
+                restaurants: restaurants(api)
+            }
+        })
+        // our store is set up to run api.loadRestaurants which, in our stub, does not resolve
+        store.dispatch('restaurants/load')
+        // while our loadRestaurants call is yet to resolve, loading should equal true
+        expect(store.state.restaurants.loading).toEqual(true)
+    })
+
+	describe('when loading succeeds', () => {
 		it('stores the restaurants', async () => {
 			const records = [
 				{id: 1, name: 'Sushi Place'},
