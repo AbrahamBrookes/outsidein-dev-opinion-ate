281f3b299e8bc6da5d86f78ef9ad7ff85784b881
Abraham Brookes <abrahambrookes@gmail.com>
2021-03-07 21:19:00 +1000

    added loading state and unit tests

--- src/store/modules/restaurants.js
+++ src/store/modules/restaurants.js
@@ -3,17 +3,23 @@ const restaurants = api => ({
 	route: '/restaurants',
 	state: {
 		records: [],
+        loading: false,
 	},
 	actions: {
 		load({commit}) {
+            commit('startLoading')
 			api.loadRestaurants().then(records => {
 				commit('storeRecords', records)
 			})
 		},
 	},
 	mutations: {
+        startLoading(state){
+            state.loading = true
+        },
 		storeRecords(state, records) {
 			state.records = records
+            state.loading = false
 		},
 	},
 })
--- tests/unit/store/modules/restaurants.spec.js
+++ tests/unit/store/modules/restaurants.spec.js
@@ -7,6 +7,17 @@ describe('restaurants', () => {
 	const localVue = createLocalVue()
 	localVue.use(Vuex)
 
+    describe('initial state', () => {
+        it('does not have the loading flag set', () => {
+            const store = new Vuex.Store({
+                modules: {
+                    restaurants: restaurants()
+                }
+            })
+            expect(store.state.restaurants.loading).toEqual(false)
+        })
+    })
+
     describe('while loading', () => {
         // create a store witha stubbed api
         const api = {
@@ -25,25 +36,30 @@ describe('restaurants', () => {
     })
 
 	describe('when loading succeeds', () => {
-		it('stores the restaurants', async () => {
-			const records = [
-				{id: 1, name: 'Sushi Place'},
-				{id: 2, name: 'Pizza Place'},
-			]
+        const records = [
+            {id: 1, name: 'Sushi Place'},
+            {id: 2, name: 'Pizza Place'},
+        ]
+        let store
 
-			const api = {
-				loadRestaurants: () => Promise.resolve(records),
-			}
+        beforeEach(() => {
+            const api = {
+                loadRestaurants: () => Promise.resolve(records),
+            }
+            store = new Vuex.Store({
+                modules: {
+                    restaurants: restaurants(api),
+                },
+            })
+            return store.dispatch('restaurants/load')
+        })
 
-			const store = new Vuex.Store({
-				modules: {
-					restaurants: restaurants(api),
-				},
-			})
-
-			await store.dispatch('restaurants/load')
-
+		it('stores the restaurants', () => {
 			expect(store.state.restaurants.records).toEqual(records)
 		})
+
+        it('clears the loading flag', () => {
+            expect(store.state.restaurants.loading).toEqual(false)
+        })
 	})
 })
