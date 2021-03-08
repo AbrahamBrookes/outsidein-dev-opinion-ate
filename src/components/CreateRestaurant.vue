986c9ac3a63b108c07f8ae716b33a5a8c2cf8d22
Abraham Brookes <abrahambrookes@gmail.com>
2021-03-08 22:24:09 +1000

    added grid styling to create restaurants form

--- src/components/CreateRestaurant.vue
+++ src/components/CreateRestaurant.vue
@@ -1,26 +1,33 @@
 <template>
     <form @submit.prevent="saveNewRestaurant()">
-        <v-text-field 
-            placeholder="Add Restaurant"
-            filled
-            type="text"
-            v-model="name"
-            data-testid="new-restaurant-name-field"
-        />
-        <div data-testid="new-restaurant-name-error" v-if="validationError">
-            Please enter a restaurant name
-        </div>
-        <div data-testid="new-restaurant-server-error" v-if="serverError">
-            There has been an error when saving your new restaurant. Please try again.
-        </div>
-        <v-btn
-            color="primary"
-            class="black--text"
-            type="submit"
-            data-testid="new-restaurant-submit-button"
-        >
-            Add
-        </v-btn>
+        <v-row>
+            <v-col cols="9">
+                <v-text-field 
+                    placeholder="Add Restaurant"
+                    filled
+                    type="text"
+                    v-model="name"
+                    data-testid="new-restaurant-name-field"
+                />
+                <div data-testid="new-restaurant-name-error" v-if="validationError">
+                    Please enter a restaurant name
+                </div>
+                <div data-testid="new-restaurant-server-error" v-if="serverError">
+                    There has been an error when saving your new restaurant. Please try again.
+                </div>
+            </v-col>
+            <v-col cols="3">
+                <v-btn
+                    color="primary"
+                    class="black--text"
+                    type="submit"
+                    block
+                    data-testid="new-restaurant-submit-button"
+                >
+                    Add
+                </v-btn>
+            </v-col>
+        </v-row>
     </form>
 </template>
 
@@ -41,7 +48,7 @@ export default {
 
         saveNewRestaurant(){
             this.serverError = false
-            
+
             if(this.name){
                 this.validationError = false
 
