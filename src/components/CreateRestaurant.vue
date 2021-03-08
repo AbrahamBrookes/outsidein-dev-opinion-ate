976557a045a4649102526c6f3f5bb31da0d020eb
Abraham Brookes <abrahambrookes@gmail.com>
2021-03-08 20:27:37 +1000

    added create restaurant component, e2e and unit tests

---
+++ src/components/CreateRestaurant.vue
@@ -0,0 +1,43 @@
+<template>
+    <form @submit.prevent="saveNewRestaurant()">
+        <v-text-field 
+            placeholder="Add Restaurant"
+            filled
+            type="text"
+            v-model="name"
+            data-testid="new-restaurant-name-field"
+        />
+        <v-btn
+            color="primary"
+            class="black--text"
+            type="submit"
+            data-testid="new-restaurant-submit-button"
+        >
+            Add
+        </v-btn>
+    </form>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+export default {
+    data() {
+        return {
+            name: '',
+        }
+    },
+    methods: {
+        ...mapActions({
+            createRestaurant: 'restaurants/create',
+        }),
+
+        saveNewRestaurant(){
+            this.createRestaurant(this.name)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ tests/e2e/specs/creating-a-restaurant.spec.js
@@ -0,0 +1,38 @@
+describe('creating a restaurant', () => {
+    it('allows adding restaurants', () => {
+        const restaurantId = 27
+        const restaurantName = 'Sushi Place'
+
+        cy.server({force404: true})
+        
+        cy.route({
+            method: 'GET',
+            url:
+                'https://outside-in-dev-api.herokuapp.com/1xYU87pyIfSPT9t9Pwrlv2QooZowK3iQ/restaurants',
+            response: [],
+        })
+
+        cy.route({
+            method: 'POST',
+            url: 
+                'https://outside-in-dev-api.herokuapp.com/1xYU87pyIfSPT9t9Pwrlv2QooZowK3iQ/restaurants',
+            response: {
+                id: restaurantId,
+                name: restaurantName,
+            }
+        }).as('addRestaurant')
+
+        cy.visit('/')
+
+        cy.get('[placeholder="Add Restaurant"]').type(restaurantName)
+        cy.contains('Add').click()
+
+        cy.wait('@addRestaurant')
+            .its('requestBody')
+            .should('deep.equal', {
+                name: restaurantName,
+            })
+
+        cy.contains(restaurantName)
+    })
+})
